c1479dc2b50db21f6f06b5710396a42a393cd90e
seunghun yang <yabby1997@gmail.com>
2021-03-17 17:25:51 +0900

    Custom TextField, Label extension으로 코드 재사용성 증가

--- InstagramClone/Controller/Authentication/LoginController.swift
+++ InstagramClone/Controller/Authentication/LoginController.swift
@@ -17,28 +17,16 @@ class LoginController: UIViewController {
     }()
     
     private let emailTextField: UITextField =  {
-        let textField = UITextField()
-        textField.borderStyle = .none
-        textField.textColor = .white
-        textField.keyboardAppearance = .dark
+        let textField = CustomTextField(placeHolder: "Email")
         textField.keyboardType = .emailAddress
-        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
-        textField.setHeight(50)
-        textField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
         
         return textField
     }()
     
     private let passwordTextField: UITextField =  {
-        let textField = UITextField()
-        textField.borderStyle = .none
-        textField.textColor = .white
-        textField.keyboardAppearance = .dark
-        textField.keyboardType = .emailAddress
-        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
-        textField.setHeight(50)
-        textField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
-        
+        let textField = CustomTextField(placeHolder: "Password")
+        textField.isSecureTextEntry = true
+            
         return textField
     }()
     
@@ -56,26 +44,14 @@ class LoginController: UIViewController {
     
     private let forgotPassword: UIButton = {
         let button = UIButton(type: .system)
-        let attributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
-        let boldAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
-        
-        let attributedTitle = NSMutableAttributedString(string: "Forgot your password?  ", attributes: attributes)
-        attributedTitle.append((NSMutableAttributedString(string: "Get help signing in.", attributes: boldAttributes)))
-        
-        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.attributedTitle(firstPart: "Forgot your password?", secondPart: "Get help signing in.")
         
         return button
     }()
     
     private let dontHaveAccountButton: UIButton = {
         let button = UIButton(type: .system)
-        let attributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
-        let boldAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
-        
-        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ", attributes: attributes)
-        attributedTitle.append((NSMutableAttributedString(string: "Sign Up", attributes: boldAttributes)))
-        
-        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.attributedTitle(firstPart: "Don't have an account?", secondPart: "Sign Up")
         
         return button
     }()
---
+++ InstagramClone/View/CustomTextField.swift
@@ -0,0 +1,31 @@
+//
+//  CustomTextField.swift
+//  InstagramClone
+//
+//  Created by Seunghun Yang on 2021/03/17.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    // MARK: -
+    init(placeHolder: String) {
+        super.init(frame: .zero)
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        leftView = spacer
+        leftViewMode = .always
+        
+        borderStyle = .none
+        textColor = .white
+        keyboardAppearance = .dark
+        backgroundColor = UIColor(white: 1, alpha: 0.1)
+        setHeight(50)
+        attributedPlaceholder = NSAttributedString(string: placeHolder, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }    
+}
