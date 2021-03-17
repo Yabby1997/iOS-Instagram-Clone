cfbab480d19ba0974dabecc5915c800d81c4eebb
seunghun yang <yabby1997@gmail.com>
2021-03-17 14:12:46 +0900

    콜렉션뷰를 코드로 구성하기

--- InstagramClone/Controller/FeedController.swift
+++ InstagramClone/Controller/FeedController.swift
@@ -7,12 +7,41 @@
 
 import UIKit
 
-class FeedController: UIViewController {
+class FeedController: UICollectionViewController {
+    // MARK: - Properties
+    let reuseIdentifier = "Cell"
     
     // MARK: - View Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+// MARK: - UICollectionViewDataSource Methods
+extension FeedController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+}
+
+// MARK: -
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
     }
 }
