000ee97197596cac972153a1adc0f9e2cb3e6ee1
seunghun yang <yabby1997@gmail.com>
2021-03-17 16:59:24 +0900

    로그인 뷰 구성

---
+++ InstagramClone/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationController.swift
+//  InstagramClone
+//
+//  Created by Seunghun Yang on 2021/03/17.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - View Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
