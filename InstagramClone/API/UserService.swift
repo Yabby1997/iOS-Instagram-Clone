07fdce71bd980fed6194d850c097e7272e155948
seunghun yang <yabby1997@gmail.com>
2021-03-28 21:08:44 +0900

    Constant.swift 추가

---
+++ InstagramClone/API/UserService.swift
@@ -0,0 +1,14 @@
+//
+//  UserService.swift
+//  InstagramClone
+//
+//  Created by Seunghun Yang on 2021/03/28.
+//
+
+import Foundation
+
+struct UserService {
+    static func fetchUser() {
+        
+    }
+}
